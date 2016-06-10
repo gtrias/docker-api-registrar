ee701ec2c459b94da66eadcd9303d45b3a7b3079
Genar Trias Ortiz <genar@acs.li>
2016-06-10 20:15:02 +0200

    successful catched a container creation

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-  console.log("Hello world!");
+var Dockerode = require('dockerode');
+var DockerEvents = require('docker-events');
+
+var options = {
+    socketPath: '/var/run/docker.sock'
+};
+
+var emitter = new DockerEvents({
+    docker: new Dockerode(options),
+});
+
+emitter.start();
+
+emitter.on("start", function(message) {
+    console.log("container started: %j", message);
+});
