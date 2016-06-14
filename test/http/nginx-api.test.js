626e22a6b63d06272f08455b87ff6ab4e7115daf
Genar Trias Ortiz <genar@acs.li>
2016-06-14 09:52:13 +0200

    first test, failing with Error: timeout of 2000ms exceeded. Error: timeout of 2000ms exceeded.

---
+++ test/http/nginx-api.test.js
@@ -0,0 +1,15 @@
+var NginxApi = require('../../lib/http/nginx-api.js');
+var expect = require('chai').expect();
+
+describe("NginxApi", function(done){
+   describe("#populatePayload()", function(){
+       it("Should convert container json to valid json payload ready to POST", function(){
+           var hosts = {};
+           var container = {};
+
+           var payload = NginxApi.populatePayload(hosts, container);
+           expect(payload).to.be(undefined);
+           dome();
+       });
+   });
+});
